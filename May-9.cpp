9ef41099f47a62e62e2c19a3b86b67ff65bbc342
suyash0103 <suyashghuge@gmail.com>
2020-05-09 13:11:40 +0530

    may 9

---
+++ May-9.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int left = 1, right = num;
+        while(left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if(pow(mid, 2) == num)
+                return true;
+            if(pow(mid, 2) > num)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        return false;
+        
+        // int k = 1;
+        // while(true)
+        // {
+        //     if(pow(k, 2) == num)
+        //         return true;
+        //     if(pow(k, 2) > num)
+        //         return false;
+        //     if(pow(k, 2) < num)
+        //         k++;
+        // }
+        // return false;
+    }
+};
