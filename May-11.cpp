2527447ee5f2462e58cb18798cf44cb1e1b57bfb
suyash0103 <suyashghuge@gmail.com>
2020-05-11 14:59:45 +0530

    may 11

--- May-11.cpp
+++ May-11.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    void fill(vector<vector<int>>& image, vector<vector<int>>& visited, int sr, int sc, int m, int n, int newColor, int oldColor)
+    void fill(vector<vector<int>>& image, int sr, int sc, int m, int n, int newColor, int oldColor)
     {
         if(sr < 0 || sr >= m || sc < 0 || sc >= n)
         {
             return;
         }
         
-        if(visited[sr][sc] == 1)
-            return;
-        
-        visited[sr][sc] = 1;
         image[sr][sc] = newColor;
         
-        if(sr - 1 >= 0 && image[sr - 1][sc] == oldColor && visited[sr - 1][sc] != 1)
+        if(sr - 1 >= 0 && image[sr - 1][sc] == oldColor)
         {
-            fill(image, visited, sr - 1, sc, m, n, newColor, oldColor);
+            fill(image, sr - 1, sc, m, n, newColor, oldColor);
         }
-        if(sr + 1 < m && image[sr + 1][sc] == oldColor && visited[sr + 1][sc] != 1)
+        if(sr + 1 < m && image[sr + 1][sc] == oldColor)
         {
-            fill(image, visited, sr + 1, sc, m, n, newColor, oldColor);
+            fill(image, sr + 1, sc, m, n, newColor, oldColor);
         }
-        if(sc - 1 >= 0 && image[sr][sc - 1] == oldColor && visited[sr][sc - 1] != 1)
+        if(sc - 1 >= 0 && image[sr][sc - 1] == oldColor)
         {
-            fill(image, visited, sr, sc - 1, m, n, newColor, oldColor);
+            fill(image, sr, sc - 1, m, n, newColor, oldColor);
         }
-        if(sc + 1 < n && image[sr][sc + 1] == oldColor && visited[sr][sc + 1] != 1)
+        if(sc + 1 < n && image[sr][sc + 1] == oldColor)
         {
-            fill(image, visited, sr, sc + 1, m, n, newColor, oldColor);
+            fill(image, sr, sc + 1, m, n, newColor, oldColor);
         }
     }
     
@@ -39,7 +35,10 @@ public:
         
         int oldColor = image[sr][sc];
         
-        fill(image, visited, sr, sc, m, n, newColor, oldColor);
+        if(oldColor == newColor)
+            return image;
+        
+        fill(image, sr, sc, m, n, newColor, oldColor);
         return image;
     }
 };
